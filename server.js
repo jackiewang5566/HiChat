b41cfc827c7bea63bba04fddd3a25ee0be0a799e
Di Wang <jackiewang5566@hotmail.com>
2017-09-11 22:56:56 -0400

    install socket.io and create basic folder structure

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+var button = document.getElementById('sendBtn');
 var express = require('express');
 var http = require('http');
+var socketIO = require('socket.io')
+var io = socketIO.listen(server);
 var app = express();
-
-// var server = http.createServer(function (req, res) {
-//     res.writeHead(200, {
-//         'Content-Type': 'text/html'
-//     });
-//     res.write('<h1>Hello World</h1>');
-//     res.end();
-// });
-
 var server = http.createServer(app);
 
 app.use('/', express.static(__dirname + '/www'));
 
 server.listen(8080);
+
+io.on('connection', function (socket) {
+    socket.on('Hello', function (data) {
+        console.log(data);
+    });
+})
+
 console.log('server started');
