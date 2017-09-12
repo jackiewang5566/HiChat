19fe09ceac191bae84f7c196ae0bdab09bd02b1f
Di Wang <jackiewang5566@hotmail.com>
2017-09-11 22:18:14 -0400

    install express and returing a static html inside express

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+var express = require('express');
 var http = require('http');
+var app = express();
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('Hi Chat!');
-    res.end();
-});
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/html'
+//     });
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// });
+
+var server = http.createServer(app);
+
+app.use('/', express.static(__dirname + '/www'));
 
 server.listen(8080);
 console.log('server started');
