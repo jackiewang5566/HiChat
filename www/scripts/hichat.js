15d65af80caa1612b360f9dff904e02a36b59a0e
Di Wang <jackiewang5566@hotmail.com>
2017-09-13 20:11:32 -0400

    send login event to server

--- www/scripts/hichat.js
+++ www/scripts/hichat.js
@@ -13,12 +13,26 @@ HiChat.prototype = {
         var that = this;
         // create connect to server
         this.socket = io.connect();
+        
         // listen connect event
         this.socket.on('connect', function () {
-            // show nick name input after connection
+            // show nick name input after connect to server
             document.getElementById('info').textContent = 'get yourself a nickname :)';
             document.getElementById('nickWrapper').style.display = 'block';
             document.getElementById('nicknameInput').focus();
-        })
+        });
+
+        // bind event listener on loginBtn
+        document.getElementById('loginBtn').addEventListener('click', function () {
+            var nickName = document.getElementById('nicknameInput').value;
+            // check input field is empty or not
+            if (nickName.trim().length != 0) {
+                // if it's not empty, trigger a login event and send nickname to server
+                that.socket.emit('login', nickname);
+            } else {
+                // if it's empty
+                document.getElementById('nicknameInput').focus();
+            }
+        }, false);
     }
 }
