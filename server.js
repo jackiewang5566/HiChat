842dc7861742def3c2a09ee45b74cfabb7d56a94
Di Wang <jackiewang5566@hotmail.com>
2017-09-13 23:32:36 -0400

    add disconnect event at server side for user leaves chat

--- server.js
+++ server.js
@@ -14,16 +14,25 @@ server.listen(80);
 io.on('connection', function (socket) {
     socket.on('login', function (nickname) {
         if (users.indexOf(nickname) > -1) {
+            // if user exist
             socket.emit('nickExisted');
         } else {
+            // if user not exist, login success
             socket.userIndex = users.length;
             socket.nickname = nickname;
             users.push(nickname);
             socket.emit('loginSuccess');
             // send nickname to all connected client
-            io.sockets.emit('system', nickname);
+            io.sockets.emit('system', nickname, users.length, 'login');
         }
     });
+
+    socket.on('disconnect', function () {
+        // remove user from users if user disconnect
+        users.splice(socket.userIndex, 1);
+        // notify everybody except the one just leaves
+        socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+    })
 })
 
 console.log('server started');
